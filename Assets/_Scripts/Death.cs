eba4650f7165c81958d5a3f26101f4cb0ceaec50
DickermanJosh <123334042+DickermanJosh@users.noreply.github.com>
2024-10-07 11:53:34 -0700

    Update Death.cs to reload current scene
    Just changing the players position rolled over the current speed from last round and didn't respawn the buffs. Reloading the scene fixes both issues without changing any behavior in gameplay

--- Assets/_Scripts/Death.cs
+++ Assets/_Scripts/Death.cs
@@ -1,20 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Death : MonoBehaviour
 {
     // Reference to the player object and spawn point
-    public Transform player;
-    public Transform spawnPoint;
+    // public Transform player;
+    // public Transform spawnPoint;
 
     // Trigger event for when the player reaches the house
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
         {
-            // Move the player back to the spawn point
-            player.position = spawnPoint.position;
+            // TODO: Make this load into a fade to black transition then reload the scene
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
     }
 }
