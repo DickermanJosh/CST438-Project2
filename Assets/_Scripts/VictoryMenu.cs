1739062a674cd6a4e13de5fa16b57e7c877094ed
KobiMurakami <151149985+KobiMurakami@users.noreply.github.com>
2024-10-24 05:02:41 -0700

    Victory Screen Basics
    Created the victory screen but the buttons still don't work I'll fix it later

---
+++ Assets/_Scripts/VictoryMenu.cs
@@ -0,0 +1,92 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using _Scripts;
+using UnityEngine.UI;
+using UnityEngine;
+using System.Diagnostics;
+using System.IO;
+using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
+using TMPro;
+
+public class VictoryMenu : MonoBehaviour
+{
+
+
+    [Header("UI Elements")] 
+    public TMP_Text TimeDisplay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        decryptSave();
+
+        StreamReader sr = new StreamReader("saveCopy.txt");
+        
+        String [] times;
+        string fullList = sr.ReadToEnd();
+        sr.Dispose();
+        times = fullList.Split(',');
+
+        if(times.Length > 2) {
+            String finalTime = times[times.Length-2];
+
+            int finalTimeInt = Int32.Parse(finalTime);
+
+            TimeSpan ts = TimeSpan.FromMilliseconds(finalTimeInt);
+            string formattedFinalTime = string.Format("{0:00}:{1:00}:{2:00}.{3:00}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
+            TimeDisplay.text = formattedFinalTime;
+        }
+        else {
+            TimeDisplay.text = "Error";
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //none
+    }
+
+    public void onClickRestart() {
+        SceneManager.LoadScene("SceneTransitionTest");
+    }
+
+    public void onClickMenu() {
+        SceneManager.LoadScene("Leaderboard+MainMenu");
+    }
+
+    public void decryptSave() {
+        StreamReader sr = new StreamReader("saveFile.txt");
+        StreamWriter sw = new StreamWriter("saveCopy.txt");
+
+        while(!sr.EndOfStream) {
+            char current = (char) sr.Read();
+            current = (char) (current - 10);
+            sw.Write(current);
+        }
+
+        sr.Dispose();
+        sw.Dispose();
+
+        File.WriteAllText("saveFile.txt", string.Empty);
+    }
+
+    public void encryptSave() {
+        StreamReader sr = new StreamReader("saveCopy.txt", true);
+        StreamWriter sw = new StreamWriter("saveFile.txt", true);
+
+        while(!sr.EndOfStream) {
+            char current = (char) sr.Read();
+            current = (char) (current + 10);
+            sw.Write(current);
+        }
+
+        sr.Dispose();
+        sw.Dispose();
+
+        File.WriteAllText("saveCopy.txt", string.Empty);
+    }
+}
